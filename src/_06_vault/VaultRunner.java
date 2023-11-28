785d7204deb4678a4a5c88d9544b3b0f0f22502e
league <karinabriellen@gmail.com>
2023-11-27 16:59:21 -0800

    am on vault

--- src/_06_vault/VaultRunner.java
+++ src/_06_vault/VaultRunner.java
@@ -3,8 +3,11 @@ package _06_vault;
 public class VaultRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+Vault v = new Vault();
+v.tryCode(12345);
+// add JOption
+int secret = 12345;
+	System.out.println(secret);
 	}
 
 }
