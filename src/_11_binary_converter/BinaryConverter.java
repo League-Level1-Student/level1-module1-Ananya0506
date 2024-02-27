cb6d93d35c50fcbc3250664fcec7f1b7baae68aa
league <league@192.168.1.72>
2024-02-26 16:40:49 -0800

    finished!

--- src/_11_binary_converter/BinaryConverter.java
+++ src/_11_binary_converter/BinaryConverter.java
@@ -1,16 +1,45 @@
 package _11_binary_converter;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BinaryConverter {
-
+public class BinaryConverter implements ActionListener {
+JFrame frame;
+JPanel panel;
+JTextField answer;
+JLabel label;
+JButton button;
 	public static void main(String[] args) {
-		JFrame frame= new JFrame();
-		JPanel panel= new JPanel();
-		JTextField answer = new JTextField(20);
+		BinaryConverter binary = new BinaryConverter();
+		binary.run();
+				
+	}
+		public void run() {
+			frame = new JFrame();
+			frame.setSize(800,800);
+			frame.setVisible(true);
+			panel = new JPanel();
+			panel.setSize(800,800);
+			answer = new JTextField(20);
+		 label = new JLabel();
+		 button = new JButton("convert");
+			panel.add(answer);
+			panel.add(label);
+			panel.add(button);
+			frame.add(panel);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			button.addActionListener(this);
+		
+		}
+		
 		String convert(String input) {
 		    if(input.length() != 8){
 		        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
@@ -30,9 +59,17 @@ public class BinaryConverter {
 		        return "-";
 		    }
 		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton buttonPressed = (JButton) e.getSource();
+			String code = answer.getText();
+			convert(code);
+			System.out.println(convert(code));
+	
+		}
 
 		                                  
 
 	}
 
-}
+
